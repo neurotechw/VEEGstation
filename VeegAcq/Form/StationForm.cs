27b5722d3b1e166b1ef552661c65b43385152557
zhangting <zhangting@neurotech.cn>
2016-06-21 11:21:43 +0800

    给工程添加日志

--- VeegAcq/Form/StationForm.cs
+++ VeegAcq/Form/StationForm.cs
@@ -34,7 +34,7 @@ namespace VeegStation
         public StationForm()
         {
             InitializeComponent();
-            
+            //LogHelper.WriteLog(typeof(StationForm),  "测试日志是否添加成功");
         }
 
         private void HelpAbout_Click(object sender, EventArgs e)
